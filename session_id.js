4208a36ef8c3f9c348f06c7caa2f97463cf74407
PATHUM LAKSHAN <121713502+Klmodz@users.noreply.github.com>
2024-12-23 17:04:14 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&6QdXiTCa#CH-vmGjGIXWtjrsVxSBNW70X8HJinlYXk9Az0gZAv50",
 };
